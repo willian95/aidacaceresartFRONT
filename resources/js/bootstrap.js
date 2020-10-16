4fd63c1bf0f0e9d685094c2624d0b4759e10178e
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-15 21:46:13 -0400

    cambios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,9 @@
 window._ = require('lodash');
 
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
  */
 
 try {
@@ -12,6 +12,12 @@ try {
 
     require('bootstrap');
 } catch (e) {}
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
 
 window.axios = require('axios');
 
