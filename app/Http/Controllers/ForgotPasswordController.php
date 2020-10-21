8b9ea95ca66dde0118686d32d96d9a71ac6dc84c
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-21 12:35:34 -0400

    cambio

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ForgotPasswordRequest;
+use App\Http\Requests\UpdatePasswordRequest;
+use Illuminate\Support\Str;
+use App\User;
 
 class ForgotPasswordController extends Controller
 {
