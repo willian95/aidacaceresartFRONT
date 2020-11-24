ae2273c90d7681cf3850f3b836e0058808509c1b
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-11-23 20:59:12 -0400

    cambios

--- resources/views/emails/purchaseEmail.blade.php
+++ resources/views/emails/purchaseEmail.blade.php
@@ -115,9 +115,9 @@ td .es-button-border-2:hover {
                       
                         <tr style="border: 1px solid black; border-collapse:collapse">
                             <td style="border: 1px solid black; border-collapse:collapse; text-align:center">{{ $loop->index + 1 }}</td>
-                            <td style="border: 1px solid black; border-collapse:collapse; text-align:center"><img src="{{ env('CMS_URL').'/images/products/'.$product['productFormatSize']['product']['image'] }}" style="width: 60px;"></td>
+                            <td style="border: 1px solid black; border-collapse:collapse; text-align:center"><img src="{{ $product['productFormatSize']['product']['image'] }}" style="width: 60px;"></td>
                             <td style="border: 1px solid black; border-collapse:collapse; text-align:center">{{ $product["productFormatSize"]["product"]["name"] }} {{ $product["productFormatSize"]["format"]["name"] }} {{ $product["productFormatSize"]["size"]["width"] }}cm / {{ $product["productFormatSize"]["size"]["height"] }}cm </td>
-                            <td style="border: 1px solid black; border-collapse:collapse; text-align:center">$ {{ number_format($product["price"], 0, ",", ".") }}</td>
+                            <td style="border: 1px solid black; border-collapse:collapse; text-align:center">$ {{ number_format($product["productFormatSize"]["price"], 0, ",", ".") }}</td>
                         </tr>
                       @endforeach
                       
